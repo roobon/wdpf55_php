962d7a5d2d79e95ad20cee858ca1d3ee759575f6
rockRayhan <khayrulalamdict@gmail.com>
2023-07-08 12:53:49 +0600

    file up

---
+++ rayhan_1277668/php_basic.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "welcome to our journey with PHP" ;
+        echo "welcome to our journey with PHP" ;
+        echo "welcome to our journey with PHP  "  ; //short open tag 
+        echo "welcome to our journey with PHP  "  ; //short open tag 
+    ?>
+
+    <?
+    echo "short open tag" ;
+    ?>
+
+    <?= "Hello this is short circuit" ;
+    ?>
+
+    
+    
+
+</body>
+</html>
