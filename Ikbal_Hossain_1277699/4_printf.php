dea26225015af86a1ad4e8beeb443be211bff88e
ikbalhossainn <ikbalhossain125@gmail.com>
2023-07-09 12:46:42 +0600

    updated

---
+++ Ikbal_Hossain_1277699/4_printf.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $districts_number = 64;
+
+        printf("Bangladesh has %d District", $districts_number);
+
+        echo "<br>";
+        printf("Today's temparature is %d and yesteday was %.2f", 33, 34.5);
+        echo "<br>";
+        printf("Today's temparature is %f and yesteday was %d", 33, 34.5);
+        echo "<br>";
+
+        printf("%s is a good boy", "Masum");
+    
+    
+    ?>
+</body>
+</html>
