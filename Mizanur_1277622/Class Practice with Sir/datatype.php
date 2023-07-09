6f68ee3a6058b06fb0932caec988335507a8de8d
Mizanur Rahman <mizanur1326@gmail.com>
2023-07-09 13:03:59 +0600

    class practice

--- Mizanur_1277622/Class Practice with Sir/datatype.php
+++ Mizanur_1277622/Class Practice with Sir/datatype.php
@@ -30,6 +30,42 @@
     $z = true;
     var_dump($z);
     echo gettype($z);
+    echo"<br>";
+
+    //Array Data type
+    $city = ["Dhaka", "Narayanganj", "Barisal"]; //single line array
+    var_dump($city);
+    echo gettype($city);
+    echo "<br>";
+
+    $fruits[0] = "Mango";   //Multiline array with self index declaration
+    $fruits[1] = "Jackfruit";
+    $fruits[2] = "Litchi";
+    $fruits[3] = "Orange";
+    var_dump($fruits);
+    echo gettype($fruits);
+    echo "<br>";
+
+    class Student {
+        public $name;
+        public $age;
+        public $ID;
+
+        function studentInfo(){
+            $name = $this->name;
+            $age = $this->age;
+            $ID = $this->ID;
+
+            $display = "ID " . $ID . " Name " . $name . " Age " . $age;
+            return $display;
+        }
+    }
+    $student = new Student;
+    var_dump($student);
+    echo "<br>";
+    echo $student->studentInfo();
     ?>
+
+
 </body>
 </html>
