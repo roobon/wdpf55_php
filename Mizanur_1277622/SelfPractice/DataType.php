1936e80f307e9b3ca474731472c54a1530ee379d
Mizanur Rahman <mizanur1326@gmail.com>
2023-07-09 22:54:32 +0600

    php practice

--- Mizanur_1277622/SelfPractice/DataType.php
+++ Mizanur_1277622/SelfPractice/DataType.php
@@ -1,33 +1,43 @@
 <?php 
-    // string Data type
+
+// string Data type
     $a = "Mizanur Rahman";
     var_dump($a);
     echo gettype($a);
-    echo "<br>";
+    
+    echo "\n";
+    echo "\n";
+
 
     // integer Data type
     $x = 10;
     var_dump($x); //to get Datatype of a variable with output.
     echo gettype($x); //to get Datatype only.
-    echo"<br>";
+    echo"\n";
+    echo"\n";
 
     // float Data type
     $y = 15.60;
     var_dump($y);
     echo gettype($y);
-    echo"<br>";
+    echo"\n";
+    echo"\n";
 
     //boolean Data type
     $z = true;
     var_dump($z);
     echo gettype($z);
-    echo"<br>";
+    echo"\n";
+    echo"\n";
 
     //Array Data type
     $city = ["Dhaka", "Narayanganj", "Barisal"]; //single line array
     var_dump($city);
     echo gettype($city);
-    echo "<br>";
+    echo "\n";
+    echo "\n";
+
+
 
     $fruits[0] = "Mango";   //Multiline array with self index declaration
     $fruits[1] = "Jackfruit";
@@ -35,8 +45,10 @@
     $fruits[3] = "Orange";
     var_dump($fruits);
     echo gettype($fruits);
-    echo "<br>";
+    echo "\n";
+    echo "\n";
 
+   
     class Student {
         public $name;
         public $age;
@@ -53,6 +65,8 @@
     }
     $student = new Student;
     var_dump($student);
-    echo "<br>";
-    echo $student->studentInfo();
+    echo "\n";
+    echo "\n";
+    //echo $student->studentInfo();
+
     ?>
