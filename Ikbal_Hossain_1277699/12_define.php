26ad9059911b698fbee6b32ddada4670e4dfeb66
ikbalhossainn <ikbalhossain125@gmail.com>
2023-07-10 12:35:47 +0600

    updated

---
+++ Ikbal_Hossain_1277699/12_define.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    define("USER", "Masum");
+    echo USER . " ";
+
+    define("USER1", "Sona");
+    echo USER1;
+
+    echo "<br>";
+    Const DOMAIN = "w3schools";
+    echo DOMAIN;
+
+    ?>
+</body>
+</html>
