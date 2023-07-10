75787a1ae26ca43c17c183ff09e7864b98d7d1e2
Mizanur Rahman <mizanur1326@gmail.com>
2023-07-10 13:14:10 +0600

    class practice

---
+++ Mizanur_1277622/Class Practice with Sir/escapeSequence.php
@@ -0,0 +1,16 @@
+<?php 
+$line = "This is first line. \n This is second line";
+//echo $line ."\n";
+
+$tabbedline = "Name \t Address \t Phone \t";
+//echo $tabbedline;
+
+$carrageReturn = "This is first line. \r This is second line" ."\n";
+//echo  $carrageReturn;
+
+//echo "The price is \$1200"
+
+//echo "It's $tabbedline";
+
+
+?>
