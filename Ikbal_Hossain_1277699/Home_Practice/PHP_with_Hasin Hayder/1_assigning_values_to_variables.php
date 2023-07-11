0f9dd52373a2e549ef421e626dab92dc9a967834
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-07-11 20:16:34 +0600

    updated

--- Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin Hayder/1_assigning_values_to_variables.php
+++ Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin Hayder/1_assigning_values_to_variables.php
@@ -11,7 +11,7 @@
     // echo "The sum of variable age = $age + '15' = " . $age + "15";
 ?>
 
-<h3> (02) Assigning values to variables </h3>
+<h3> (02) Various ways to assign values to variables </h3>
 <?php 
     $name = "Bangladesh";
     $age = 16;
@@ -24,6 +24,24 @@
     echo "<br>";
 
     echo "3 Hello" . " " . "Bangladesh";
+    echo "<br>";
+
+    echo "4 Hello " .$name;
+    echo "<br>";
+
+    echo "5 Hello $name";
+    echo "<br>";
+
+    echo '6 Hello $name'; // Hello $name >= single quotation will consider "string" here.
+    echo "<br>";
+
+    echo "7 Hello {$name}";
+    echo "<br>";
+
+    echo "8 Hello {$name}, {$question}"; // Looking good/
+    echo "<br>";
+
+    echo "9 Hello " . $name . ", " . $question; // Looks not fine
     echo "<br>";
 
 
