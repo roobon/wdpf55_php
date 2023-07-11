0857742b2daf84d7c9cc10a92655b307895d1c9e
unknown <mahdiulh@gmail.com>
2023-07-11 13:09:12 +0600

    add

---
+++ MahdiulHaque_1274133/cls-04/for.php
@@ -0,0 +1,20 @@
+<?php 
+ 
+ for($x = 1; $x<=10; $x++){
+    echo "The Number is " . $x .  "<br>" ;
+ }
+
+
+?>
+
+<?php 
+     $days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+     
+     for ($i=0; $i <count($days) ; $i++) { 
+         echo $days[$i] . "<br>";
+     
+     }
+     
+
+
+?>
