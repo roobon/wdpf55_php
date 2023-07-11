7986a23b0f09d56906ab201423eb623b922705b5
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-07-11 16:00:57 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin Hayder/1_assigning_values_to_variables.php
@@ -0,0 +1,11 @@
+<h3> Assigning values to variables </h3>
+
+<?php 
+    $color = "red";
+    $number = 12;
+    $age = 12;
+    $sum = $age + "15";
+
+    echo "The sum of variable age = $age + '15' = " . $age . 15
+
+?>
