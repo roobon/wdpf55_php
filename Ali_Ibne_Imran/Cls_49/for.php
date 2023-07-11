cd6faa9eaa18d6f676f0f8141c762dc6cb9fbbc9
unknown <you@example.com>
2023-07-11 13:07:05 +0600

    add

---
+++ Ali_Ibne_Imran/Cls_49/for.php
@@ -0,0 +1,14 @@
+
+ <?php
+    for($x = 1; $x<=10; $x++){
+        echo $x." ";
+    }
+ ?>
+ <br>
+ <?php 
+    $days = ["Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Satarday"];
+    // echo count($days);
+    for($x =0; $x<count($days); $x++){
+        echo $days[$x] ."<br>";
+    }
+ ?>
