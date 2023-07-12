bfd97acd691d7dc312c1ad74178a1057c9eb991f
masum-yasin <masum55549@gmail.com>
2023-07-12 13:34:40 +0600

    update

---
+++ Masum_1273758/Home_Exercise/condition.php
@@ -0,0 +1,12 @@
+<?php 
+$age =25;
+if($age>=45 && $age>20){
+    echo("This person is teenager");
+}
+elseif($age>45 ||$age<20){
+    echo("this person is not teenger");
+}
+else{
+   echo ("you are young");
+}
+?>
