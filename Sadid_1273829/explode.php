68cb0ba668c98d7a1c8d6cb43d03c5eda0ac3904
Your NameMamun876 <you@example.commamunrrashid876@gmail.com>
2023-07-13 13:18:38 +0600

    commit

---
+++ Sadid_1273829/explode.php
@@ -0,0 +1,6 @@
+<?php
+$txt = "We have mid month exam on 16 july";
+$array = explode(" ", $txt);
+echo "<pre>";
+echo print_r($array);
+?>
---
+++ Sadid_1273829/readTextData.php
@@ -0,0 +1,11 @@
+<?php
+
+$users = file("users.txt");
+echo "<pre>";
+print_r($users);
+
+foreach ($users as $user) {
+    list($name, $email, $phone) = explode("|", $user);
+    echo "Name : $name, Email: $email, Phone Number: $phone";
+}
+?>
