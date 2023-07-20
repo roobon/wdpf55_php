3e868c1439c1ad3a7463eb4aa3d867bc5d54b077
masum-yasin <masum55549@gmail.com>
2023-07-20 14:59:17 +0600

    update

---
+++ Masum_1273758/Home_Exercise/inheretise.php
@@ -0,0 +1,15 @@
+<?php 
+class garndFather{
+    public $name="khayrul Alam Rayan";
+    public function getName(){
+        return $this->name;
+    }
+   
+   
+}
+class daddy extends grandFaName{
+    public function displaygrandFaName(){
+        return $this->name;
+    }
+}
+?>
