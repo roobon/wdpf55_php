dbb034ce5191d1fc5398810f29a502cc39c3319f
ikbalhossainn <ikbalhossain125@gmail.com>
2023-07-20 11:43:32 +0600

    updated

--- Ikbal_Hossain_1277699/Class_Practice/Array/40_key_next.php
+++ Ikbal_Hossain_1277699/Class_Practice/Array/40_key_next.php
@@ -5,9 +5,45 @@
     next($countries);
     echo "<br>";
     echo key($countries);
+    echo "<hr>";  
+?>
 
+
+<h3>Show the keys using while loop </h3>
+<?php 
     while($key = key($countries)){
-
+        echo "$key <br>";
+        next($countries);
     }
+    echo "<hr>"; 
+?>
 
+
+<h3>Show the keys using for each loop </h3>
+<?php 
+    foreach ($countries as $city=> $country) {
+        echo $city . "<br>"; 
+    }
+    echo "<hr>"; 
+?>
+
+
+<h3>Show the values using while loop </h3>
+<?php 
+    $countries = array("Dhaka"=> "Bangladesh", "kathmundu"=> "Nepal", "Colombo"=> "Srilanka", "Maley"=> "Maldives");
+
+    while($value = current($countries)){
+        echo "$value <br>";
+        next($countries);
+    }
+    echo "<hr>"; 
+?>
+
+
+<h3>Show the values using for each loop </h3>
+<?php 
+     foreach($countries as $city=> $country){
+        echo $country . "<br>";
+    }
+    echo "<hr>"; 
 ?>
