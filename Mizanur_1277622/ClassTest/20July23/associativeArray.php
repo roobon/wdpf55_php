4d46586c525d5ec1ba80b8ddb5a945bb8c01607f
Mizanur Rahman <mizanur1326@gmail.com>
2023-07-20 09:34:20 +0600

    class test 20July

---
+++ Mizanur_1277622/ClassTest/20July23/associativeArray.php
@@ -0,0 +1,5 @@
+<?php 
+$name = array("Mizanur" => "Mizanur Rahman", "Iqbal" => "Iqbal Molla");
+//echo "Hi! I'm " . $name["Mizanur"];
+var_dump($name);
+?>
---
+++ Mizanur_1277622/ClassTest/20July23/numericIndexedArray.php
@@ -0,0 +1,9 @@
+<?php 
+$num = array(
+    array(10,20,30),
+    array(40,50,60),
+    array(70,80,90),
+);
+//echo $num;
+var_dump($num)
+?>
