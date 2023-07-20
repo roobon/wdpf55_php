b5ede509870d005ff2fd416401141a2d3cc22d72
unknown <kamrulislam.serzy50@gmail.com>
2023-07-20 09:22:54 +0600

    array

---
+++ Mamun_1277681/arrayKeyExist.php
@@ -0,0 +1,11 @@
+<?php 
+$ages = ["masum"=>24, "Uzzal"=> 28, "Rakib"=>31];
+if(array_key_exists("SOhan", $ages)){
+    echo "Yes! Exist";
+}
+else{
+    echo"Not Exist";
+};
+
+
+?>
---
+++ Mamun_1277681/inArray.php
@@ -0,0 +1,9 @@
+<?php 
+$numbers =[2, 50, 3]; //[2, 4, 65, 76, 34]
+if (in_array(15, $numbers)){
+    echo "Yes, you are selected";
+}
+else {
+   echo "sorry, its not include in array";
+}
+?>
