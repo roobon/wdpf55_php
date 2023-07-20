a8675ee6c09aea2e714ad06438623ac022882120
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-07-20 09:56:47 +0600

    practices

---
+++ Alauddin_1273850/associative_array.php
@@ -0,0 +1,16 @@
+<?php 
+echo "Associative Array";
+
+$associative_array = array("Rfiq"=>26, "Ikbal"=>29, "Shuvo"=>22, "Rayhan"=>24,);
+
+// $output = "";
+// foreach($associative_array as $name=>$age){
+//     //echo "Name: $name Age: $age<br>";
+//     $output .= "Name: $name Age: $age<br>";
+// }
+
+echo "<pre>";
+//var_dump($output);
+print_r($associative_array)
+
+?>
