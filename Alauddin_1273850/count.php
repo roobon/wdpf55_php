340c5fbb72adabce63ec52d2fd0aadf7a390d577
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-07-20 13:14:35 +0600

    classwork

---
+++ Alauddin_1273850/count.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $garden = array("cabbbage", "peppers", "turnips", "carrots", array("bombay Chili", "Green Chili"), "potato");
+
+    //echo count($garden);
+    echo count($garden, 1);
+    echo "<br>";
+    echo sizeof($garden, 1);
+
+
+?>
