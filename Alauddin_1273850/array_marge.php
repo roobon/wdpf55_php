18a1b71a6850861ca786ce231f49b00e1eacc12c
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-07-22 13:21:32 +0600

    classwork

---
+++ Alauddin_1273850/array_marge.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $asia = ["Bangladesh", "Nepal", "Bhutan", "Srilanka", "India"];
+    $otherworld = ["Australia", "England", "South Africa", "Newzeland", "Island"];
+
+    $marge = array_merge($asia, $otherworld);
+
+    
+
+
+
+?>
