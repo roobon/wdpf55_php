62679e17ba4d0d1e91bc4bdf05bd179e4f811d19
ikbalhossainn <ikbalhossain125@gmail.com>
2023-07-23 12:46:33 +0600

    updated

--- Ikbal_Hossain_1277699/Evidence/read_txt_data_oop.php
+++ Ikbal_Hossain_1277699/Evidence/read_txt_data_oop.php
@@ -1,6 +1,19 @@
 <?php 
     class FileClass {
         private $data;
+        function __construct($x){
+            $this->data = $x;
+        }
+        function readData(){
+            $rawData = file($this->data);
+            // print_r($rawData);
+            foreach($rawData as $item){
+                echo $item . "<br>";
+            }
+        }
         
     }
+
+    $mydata = new FileClass("users.txt");
+    $mydata->readData();
 ?>
