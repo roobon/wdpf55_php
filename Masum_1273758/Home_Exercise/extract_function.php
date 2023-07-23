d566fa9b71d9f43d10fb8708cbf0eceac0c4b5c7
mehedishubho <mehedihassanshubho@gmail.com>
2023-07-23 11:57:40 +0600

    23-07-2023

--- Masum_1273758/Home_Exercise/extract_function.php
+++ Masum_1273758/Home_Exercise/extract_function.php
@@ -1,43 +1,4 @@
 <?php 
-$student=array("name"=>"Kamrul Hasan Masum","age"=>25,"address"=>"Dhaka,Bangladesh","country"=>"Canada");
-extract($student);
-echo "Name:$name Age:$age Address: $address Country Form:$country";
-echo "<hr>";
-$student=array("Kamrul Hasan Masum",25,"Dhaka Bangladesh","Canada");
-extract($student,EXTR_PREFIX_ALL,"a");
-echo "Name: $a_0 ,Age:$a_1 Address:$a_2, Country: $a_3"."<br>";
-echo "<hr>";
-$student =[
-    ["name"=>"Khayrul Alam Rayan","age"=>25],
-    ["name"=>"Masum Alam","age"=>30,],
-    ["name"=>"Nadia sultana","age"=>30]
-];
-
-foreach($student as $singleArray){
-foreach ($singleArray as $key=>$value){
-    echo $key.":".$value."<br>";
-}
-
-}
-echo "<hr>";
-$student =[
-    ["name"=>"Khayrul Alam Rayan","age"=>25],
-    ["name"=>"Masum Alam","age"=>30,],
-    ["name"=>"Nadia sultana","age"=>30]
-];
-foreach($student as $singleArray);
-extract($singleArray);
-echo "Name:$name Age:$age";
-echo "<hr>";
-$student=[
-    ["Al Mahmud Hasan",25],
-    ["Kamrul Hasan Mia",30],
-    ["khayrul Alam Rayan",40],
-];
-foreach ($student as list($key,$value)){
-    echo $key.":".$value."<br>";
-}
-
-
+$student
 
 ?>
