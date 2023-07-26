464573d92a58723508cbd3e503cd753c4d8bbe47
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-07-26 07:38:35 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Class_Practice/MCQ_Test/1_mcq1.php
@@ -0,0 +1,11 @@
+
+<?php    
+    $num = 10;    
+    echo 'What is her age? \n She is $num years old';    
+?>
+
+<?php    
+$score = 1234;    
+$scoreboard = (array) $score;    
+echo $scoreboard[0];    
+?>
--- Ikbal_Hossain_1277699/Home_Practice/MCQ_Test/1_mcq1.php
+++
@@ -1,5 +0,0 @@
-
-<?php    
-    $num = 10;    
-    echo 'What is her age? \n She is $num years old';    
-?>
