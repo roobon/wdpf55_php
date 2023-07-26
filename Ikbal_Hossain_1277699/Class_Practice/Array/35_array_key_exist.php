45ddff820c699075094bc49576d8a81487f7d104
Ikbal Hossain <132182881+ikbalhossainn@users.noreply.github.com>
2023-07-26 13:50:15 +0600

    Update 35_array_key_exist.php

--- Ikbal_Hossain_1277699/Class_Practice/Array/35_array_key_exist.php
+++ Ikbal_Hossain_1277699/Class_Practice/Array/35_array_key_exist.php
@@ -1,4 +1,5 @@
 <?php 
+// The array_key_exists() function checks an array for a specified key, and returns true if the key exists and false if the key does not exist.
     $age = array("Peter"=> 35, "Ben"=> 37, "Joy"=> 43);
     echo array_key_exists("Ben", $$age);
     echo "<hr>"
