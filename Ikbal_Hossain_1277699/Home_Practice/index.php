8a23c3f570ca4ceaf0be6c22252f8724a320ec98
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-07-26 07:25:30 +0600

    updated

--- Ikbal_Hossain_1277699/Home_Practice/index.php
+++ Ikbal_Hossain_1277699/Home_Practice/index.php
@@ -2,6 +2,12 @@
     $price1 = 400;
     $price2 = 350;
 
-    echo $price1.$price2;
+    echo $price1 + $price2;
+    echo "<br>";
 
+?>
+
+<?php    
+    $num = 10;    
+    echo 'What is her age? \n She is $num years old';    
 ?>
