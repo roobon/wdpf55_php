a471f3844aa80344a1289c63720525d1c39da3d9
Nadia_Trisha <nadiatrisha68@gmail.com>
2023-07-27 13:21:06 +0600

    27 july 2023

---
+++ Rojoni_1274253/file_based_authentic.php
@@ -0,0 +1,14 @@
+<?php 
+// $_SERVER['PHP_AUTH_USER'] = 'Masum';
+// $_SERVER['PHP_AUTH_PW'] = '12345';
+
+
+if (! isset($_SERVER['PHP_AUTH_USER']) || ! isset($_SERVER['PHP_AUTH_PW'])) {
+header('WWW-Authenticate: Basic Realm="Authentication"');
+header("HTTP/1.1 401 Unauthorized");
+} else {
+echo "Your supplied username: {$_SERVER['PHP_AUTH_USER']}<br />";
+echo "Your password: {$_SERVER['PHP_AUTH_PW']}<br />";
+}
+
+?>
