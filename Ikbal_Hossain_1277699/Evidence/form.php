92c2387b80de122719b8e8cbca07bd3ffe46ff11
ikbalhossainn <ikbalhossain125@gmail.com>
2023-07-31 09:48:19 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Evidence/form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="action_page.php" method="POST">
+        <input type="text" name="fullname" placeholder="Enter your name"><br>
+        <input type="text" name="email" placeholder="Enter your email address"> <br>
+        <input type="number" name="phone" placeholder="Enter your phone number"> <br>
+        <textarea name="address" id="" cols="30" rows="10"></textarea> <br>
+        <input type="submit" name="submit" value="SUBMIT">
+    </form>
+</body>
+
+</html>
