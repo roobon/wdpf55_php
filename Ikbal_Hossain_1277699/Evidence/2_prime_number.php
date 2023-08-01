c450276cefd982a69d10495a5561eeabdb44ca13
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-01 11:46:45 +0600

    updated

--- Ikbal_Hossain_1277699/Evidence/2_prime_number.php
+++ Ikbal_Hossain_1277699/Evidence/2_prime_number.php
@@ -6,15 +6,34 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
-        <input type="text" name="number" placeholder="Enter a number"> <br>
-        <input type="submit" value="Submit">
-    </form>
+    <?php
+        if (isset($_REQUEST['submit'])) {
 
-    <?php 
-        // print_r($_GET);
-        // $_GET["number"];
-        echo $_GET["number"];
+            function PrimeCheck($number){
+                if ($number == 0) {
+                    return "0 is not acceptable";
+                } elseif($number == 1) {
+                    return "1 is not a prime number";
+                }elseif ($number==2) {
+                    return "2 is a prime number";
+                } else {
+                    for ($i=2; $i < $number; $i++) { 
+                        if ($number % $i == 0) {
+                            return "{$number} is not a prime number";
+                        }
+                    }
+                    return "{$number} is a prime number.";
+                }
+            } // end of function
+
+            $myNum = $_REQUEST['myNum']; // $_REQUEST['myNum'] -> eita diye form er value ta pawa jay;
+            echo PrimeCheck($myNum);
+        }
     ?>
+
+    <form action="" method="post">
+        <input type="number" name="myNum" placeholder="Enter a number">
+        <input type="submit" name="submit" value="Submit">
+    </form>
 </body>
 </html>
---
+++ Ikbal_Hossain_1277699/Evidence/9_student_grade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    if(isset($_POST["submit"])){
+        $grade = $_POST["grade"];
+
+        // $grade = array("A", "B", "C", "D", "F");
+        if ($grade == "A") {
+            echo "excellent";
+        } else if ($grade == "B") {
+            echo "good";
+        } else if ($grade == "C") {
+            echo "fair";
+        } else if ($grade == "D") {
+            echo "poor";
+        } else {
+            echo "failure";
+        }
+    }
+       
+    ?>
+
+    <form action="">
+        <input type="text" name="grade" placeholder="Enter your grade">
+        <input type="submit" name="submit" value="CHECK">
+    </form>
+</body>
+
+</html>
