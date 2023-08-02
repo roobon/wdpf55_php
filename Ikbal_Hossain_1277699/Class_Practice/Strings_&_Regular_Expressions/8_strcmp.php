f19968ac48d3bb2380b0cdd7bfc33d18e2a8218d
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-02 12:54:46 +0600

    updated

--- Ikbal_Hossain_1277699/Class_Practice/Strings_&_Regular_Expressions/8_strcmp.php
+++ Ikbal_Hossain_1277699/Class_Practice/Strings_&_Regular_Expressions/8_strcmp.php
@@ -6,4 +6,18 @@
     } else {
     echo "Passwords match!";
     }
+    echo "<hr>";
+?>
+
+<?php
+    $pswd = "supersecret";
+    $pswd2 = "supersecret2";
+
+    echo strcmp($pswd, $pswd2);
+    echo "<hr>";
+    if (strcmp($pswd, $pswd2) != 0) {
+    echo "Passwords do not match!";
+    } else {
+    echo "Passwords match!";
+    }
 ?>
