2ccac846495a56cdd87b252aed9ddbeb5933e0b4
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-08-03 12:26:18 +0600

    sge

---
+++ Alauddin_1273850/string_regEx/ucfirst_lcfirst_ucword.php
@@ -0,0 +1,20 @@
+<?php 
+
+$msg = "I annoy people by capitalizing e-mail text.";
+echo "<br>";
+echo $lower = strtolower($msg);
+echo "<br>";
+$ucfirst = ucfirst($lower);
+echo $ucfirst;
+echo "<br>";
+$upper = strtoupper($ucfirst);
+echo $upper;
+echo "<br>";
+
+
+
+
+
+
+
+?>
