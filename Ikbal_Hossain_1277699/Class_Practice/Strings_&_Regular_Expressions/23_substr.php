8fa1552b59361ef1346e6bb82b7d03f7ea97f21a
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-03 11:07:47 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Class_Practice/Strings_&_Regular_Expressions/23_substr.php
@@ -0,0 +1,19 @@
+<?php
+    $car = "1944 Ford";
+    echo substr($car, 5);
+    echo "<hr>";
+?>
+
+<?php 
+    $msg = "I annoy people by capitalizing e-mail text.";
+
+    echo substr($msg, 8);
+    echo "<hr>";
+?>
+
+<?php 
+    $msg = "I annoy people by capitalizing e-mail text.";
+
+    echo substr($msg, 8, 6);
+    echo "<hr>";
+?>
