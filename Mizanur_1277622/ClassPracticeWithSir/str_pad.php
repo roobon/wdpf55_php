6972d584cfed48fad490f39a1b70beaecf716cf5
Mizanur Rahman <mizanur1326@gmail.com>
2023-08-03 12:23:28 +0600

    03Aug23 Class

---
+++ Mizanur_1277622/ClassPracticeWithSir/str_pad.php
@@ -0,0 +1,8 @@
+<?php 
+$header = "Log Report";
+echo str_pad($header, 20, "/-") . "\n"; //str_pad add text/charecter in text 
+echo str_pad($header, 20, "/-", STR_PAD_BOTH). "\n";
+echo "\n";
+$tk = 4567;
+echo str_pad($tk, 10, STR_PAD_LEFT);
+?>
