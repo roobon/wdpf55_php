1515690e1bfb0d213869341def2ebe1977e49090
unknown <you@example.com>
2023-08-03 13:12:51 +0600

    add

---
+++ Ali_Ibne_Imran/Cls_66/basename.php
@@ -0,0 +1,4 @@
+<?php 
+    echo basename("D:\WDPF_55_WDPF\PHP\htdocs\WDPF55_PHP\Ali_Ibne_Imran\Cls_66\str_pad.php");
+
+?>
---
+++ Ali_Ibne_Imran/Cls_66/pathinfo.php
@@ -0,0 +1,6 @@
+<?php 
+    $info = pathinfo("D:\WDPF_55_WDPF\PHP\htdocs\WDPF55_PHP\Ali_Ibne_Imran\Cls_66\str_pad.php");
+    echo "<pre>";
+    print_r($info);
+
+?>
