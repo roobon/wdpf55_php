696edbeea9d7e7e689ae9fe32b7f4da9b2c5f52e
unknown <kamrulislam.serzy50@gmail.com>
2023-08-05 13:17:52 +0600

    file upload

---
+++ Mamun_1277681/strings/realpath.php
@@ -0,0 +1,6 @@
+<?php 
+
+$path = "../../MahdiulHaque_1274133/variable.php";
+echo realpath($path) ;
+echo "<br>";
+?>
---
+++ Mamun_1277681/strings/strpos.php
@@ -0,0 +1,5 @@
+<?php 
+
+$msg = "I Annoy People to Annoy People to Email Text ";
+echo strrpos($msg, 'People');
+?>
