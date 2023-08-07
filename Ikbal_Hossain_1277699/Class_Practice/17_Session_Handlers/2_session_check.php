89cefda33d0435d11955868b94a62fa014f071e5
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-07 12:35:13 +0600

    updated

--- Ikbal_Hossain_1277699/Class_Practice/17_Session_Handlers/2_session_check.php
+++ Ikbal_Hossain_1277699/Class_Practice/17_Session_Handlers/2_session_check.php
@@ -6,3 +6,27 @@
 
     echo session_id();
 ?>
+
+
+<!-- <?php 
+    // session_start();
+
+    // print_r($_SESSION);
+    // echo "<br>";
+
+    // echo session_id();
+
+    // session_destroy();
+?>
+
+
+<?php 
+    // session_start();
+
+    // print_r($_SESSION);
+    // echo "<br>";
+
+    // echo session_id();
+
+    // session_unset();
+?> -->
