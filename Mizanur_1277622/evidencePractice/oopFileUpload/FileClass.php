b96405b3b37fca44d996a5b0eadad7d993b73632
Mizanur Rahman <mizanur1326@gmail.com>
2023-08-08 13:12:21 +0600

    fileClass Update

--- Mizanur_1277622/evidencePractice/oopFileUpload/FileClass.php
+++ Mizanur_1277622/evidencePractice/oopFileUpload/FileClass.php
@@ -4,13 +4,16 @@ class  FileClass{
     public $name;
     public $size;
     public $tmpName;
+    public $ext;
     public function __construct($file){
         $this->image = $file;
+        //echo "<pre>";
+        //print_r($this->image = $file);
         $this->name = $this->image["name"];
         $this->size = $this->image["size"];
         $this->tmpName = $this->image["tmp_name"];
-        //echo "<pre>";
-        //print_r($this->image = $file);
+        $extArray = explode("." , $this->name);
+        $this->ext = strtolower(end($extArray));
     }
 
     public function doUpload(){
@@ -19,6 +22,10 @@ class  FileClass{
         $limit = 102400;
         if($this->size>$limit){
             $error[] = "Max upload size is 100kb";
+        }
+        $allowedtype = ["jpg", "jpeg", "png"];
+        if(!in_array($this->ext, $allowedtype)){
+            $error[] = "Upload failed. You should upload jpg/jpeg/png files only";
         }
         if(empty($error)){
             if(move_uploaded_file($this->tmpName, $path.$this->name)){
