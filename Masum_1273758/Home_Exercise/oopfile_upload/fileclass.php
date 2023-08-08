900373f53523f73d675dd405bac1b8b7afaaafbf
masum-yasin <masum55549@gmail.com>
2023-08-08 13:19:30 +0600

    update

--- Masum_1273758/Home_Exercise/oopfile_upload/fileclass.php
+++ Masum_1273758/Home_Exercise/oopfile_upload/fileclass.php
@@ -4,19 +4,21 @@ class FileClass{
     public $name;
     public $size;
     public $tmpName;
+    public $ext;
     public function __construct($file)
     {
         $this->image=$file;
         $this->name=$this->image['name'];
         $this->size=$this->image['size'];
         $this->tmpName=$this->image['tmp_name'];
+        $array =explode(".",$this->name);
     }
     public function doUpload(){
         $path = "uploads/";
         $errors = array();
-        $limit =102400; // 100kb
+        $limit =204800; // 200kb
         if($this->size>$limit){
-         $errors[] ="you can upload with in 100kb";
+         $errors[] ="you can upload with in 200kb";
         }
         if(empty($errors)){
             if(move_uploaded_file($this->tmpName,$path.$this->name)){
