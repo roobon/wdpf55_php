8c529cf0478942e609a25ded6e358a8b2521dcae
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-09 11:01:36 +0600

    updated

--- Ikbal_Hossain_1277699/Evidence/Show_data_another_page/action_Page.php
+++ Ikbal_Hossain_1277699/Evidence/Show_data_another_page/action_Page.php
@@ -1,51 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-    if(isset($_POST['submit'])){
-        echo $name =$_POST['name'];
-      echo "<br>";
-     echo $email= $_POST['email'];
-       echo "<br>";
-        $language = $_POST['languages'];
-        echo "<br>";
-        $hobbies = $_POST['hobbies'];
-
-       
-    //    foreach($language as $lang){
-    //     echo $lang.",";
+    <?php
+        if (isset($_POST['submit'])) {
+            echo $name = $_POST['name'];
+            echo "<br>";
+            echo $email = $_POST['email'];
+            echo "<br>";
+            $language = $_POST['languages'];
+            echo "<br>";
+            $hobbies = $_POST['hobbies'];
 
 
-    //    }
-        $langs = implode(", ",$language);
-        echo "All your favoruite languages". $langs."<br>";
-        $hobby =implode(", ",$hobbies);
-        echo "All your favoruite  hobby game".$hobby."<br>";
-    }
-    
+            //    foreach($language as $lang){
+            //     echo $lang.",";
+
+
+            //    }
+            $langs = implode(", ", $language);
+            echo "All your favorite languages: " . $langs . "<br>";
+            $hobby = implode(", ", $hobbies);
+            echo "All your favorite  hobby game: " . $hobby . "<br>";
+        }
+
     ?>
-    <form action="<?php echo  $_SERVER['PHP_SELF']?>" method="post">
-    <input type="text" name="name" placeholder="Enter your Name"><br>
-    <input type="text"name="email" placeholder="Enter your Email"><br>
-    <select name="languages[]" multiple>
-        <option value="PHP">PHP</option>
-        <option value="JAVASCRIPT">JAVASCRIPT</option>
-        <option value="REACT">REACT</option>
-        <option value="ANGULER">ANGULER</option> </select>
-        <br>
-        Hobbies <br>
-        <input type="checkbox" value="Cricket"name="hobbies[]">Cricket
-        <input type="checkbox"value="Football" name="hobbies[]">Football
-        <input type="checkbox"value="Tensi" name="hobbies[]">Tenis
-        <input type="checkbox" value="Games"name="hobbies[]">Games
-        <input type="submit"name="submit" value="CHECK">
 
-   
-</form>
+
+    <form action="<?php echo  $_SERVER['PHP_SELF'] ?>" method="post">
+        Name: <input type="text" name="name" placeholder="Enter your Name"><br>
+       Email: <input type="text" name="email" placeholder="Enter your Email"><br> <br>
+        Course: <br>
+        <select name="languages[]" multiple>
+            <option value="PHP">PHP</option>
+            <option value="JAVASCRIPT">JAVASCRIPT</option>
+            <option value="REACT">REACT</option>
+            <option value="ANGULER">ANGULER</option>
+        </select>
+        <br> <br>
+
+        Hobbies: <br>
+        Cricket<input type="checkbox" value="Cricket" name="hobbies[]">
+        Football<input type="checkbox" value="Football" name="hobbies[]">
+        Tennis<input type="checkbox" value="Tennis" name="hobbies[]">
+        Traveling<input type="checkbox" value="Games" name="hobbies[]">
+        <br>
+        <input type="submit" name="submit" value="CHECK">
+
+
+    </form>
 </body>
+
 </html>
