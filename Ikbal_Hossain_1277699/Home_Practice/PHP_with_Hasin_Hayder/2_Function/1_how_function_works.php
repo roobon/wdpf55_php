b37618f96d3122ebb09554401eb9e05b89e2d570
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-08-10 20:49:06 +0600

    updated

--- Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/1_how_function_works.php
+++ Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/1_how_function_works.php
@@ -75,9 +75,9 @@
     function evenOrOdd4($n)  // ($n) is a parameter.
     {
         if($n % 2 == 0){
-            return true;
+            return true;  
         }
-        return false;
+        return false;  // function a jodi akbar return call hoy and seta true hoy tahole porer code ar run korbe na. tai else deoyar ar dorkar nei. only return false holei hobe. karon ture hole porer code se ar jabei na
     }
 
     $x = 699;
@@ -90,4 +90,31 @@
         echo "$x is an odd number";
     }
     echo "<hr>";
+
+    // acca ei function dekhe ki buja jacce even hole true hobe or odd hole true hobe?? seta janar jonno amake code a giye check korte hocce. tai amader function ar akta meaningful name dite hobe
+?>
+
+
+<h3> More meaningful way of function declaration </h3>
+<?php 
+    // Determines if the argument is even or odd
+    function isEven($n)
+    {
+        if($n % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    $x = 1277699;
+
+    $check = isEven($x);
+
+    if(isEven($x)){  // eikhane function name dekhei buja jacce je even hole true check korbe otherwise false check korbe.
+        echo "$x is an even number";
+    } else {
+        echo "$x is an odd number";
+    }
+
+    // *** eikhane ami caile function likhar kaj tuku onno file likhe rakhte pari ja amar user ar joono proyojon nai. user ar kache sudu value input korar option thakbe. backend ar code tar janar dorkar nai => => => this concept is called encapsulation.
 ?>
