1fa120ae2a773b0db139674df3e7cbeade56aec6
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-08-12 22:34:00 +0600

    updated

--- Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/5_unlimited_argument_accept.php
+++ Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/5_unlimited_argument_accept.php
@@ -1,3 +1,46 @@
 <?php 
+    function add(int $x, int $y, int $z) :int{
+        return $x + $y + $z;
+    }
     
+    echo "Sum of the number: " . add(5, 6, 7, 8); // 8 won't be accepted as an argument
+    echo "<hr>";
 ?>
+?>
+
+
+
+<?php 
+    function add1(int $x, int $y, int $z) :int{
+        return $x + $y + $z;
+    }
+
+    $num1 = 5;
+    $num2 = 6;
+    // $num3 = 7;
+
+    $Addition = add1($num1, $num2, $num3);  // here $num3 is undefined variable
+    
+    echo "Sum of the number: $Addition";
+    echo "<hr>";
+?>
+
+
+
+<?php 
+    function sum(int $x, int $y, int $z) :int{
+        return $x + $y + $z;
+    }
+
+    $num1 = 5;
+    $num2 = 6;
+    $num3 = 7;
+
+    $Addition = sum($num1, $num2);
+    
+    echo "Sum of the number: $Addition";
+    echo "<hr>";
+    // fetal error: jokhon apni 3 ta parameter set korecen kintu argument dilen 2 ta tokhon fetal error dekhabe.
+?>
+
+
