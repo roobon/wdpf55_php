0c26844b4eb6c046cb6174facfed9da0fca454a0
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-08-12 18:59:42 +0600

    updated

--- Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/3_parameter_default_value.php
+++ Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/3_parameter_default_value.php
@@ -6,9 +6,79 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h4> Basic function </h4>
     <?php 
+        function serve($foodType, $numberOfItems){
+            echo "{$numberOfItems} of {$foodType} have  been served";
+        }
+        $ft = "Coffee";
+        $n = "2 cups";
 
+       serve($ft, $n);
+       echo "<hr>";
+    ?>
+
+<h4> Function parameter default value or optional parameter </h4>
+<?php 
+        function serve1($foodType, $numberOfItems){
+            echo "{$numberOfItems} of {$foodType} have  been served";
+        }
+
+        $ft = "Coffee";
+        // $n = "";
+
+       serve1($ft, $n);
+       echo "<hr>";
+    ?>
+
+
+<?php 
+        function serve2($foodType, $numberOfItems = "1 cup"){  // set a default parameter 
+            echo "{$numberOfItems} of {$foodType} has  been served";
+        }
+
+        $ft = "Coffee";
+
+       serve2($ft);
+       echo "<hr>";
+    ?>
+
+
+
+<?php 
+        function serve3($foodType = "Coffee", $numberOfItems = "3 cup"){  // set a default parameter 
+            echo "{$numberOfItems} of {$foodType} have  been served";
+        }
+
+       serve3();
+       echo "<hr>";
+    ?>
+
+<h4> Default parameter overwrite </h4>
+<?php 
+        function serve4($foodType, $numberOfItems = "1 cup"){  // set a default parameter 
+            echo "{$numberOfItems} of {$foodType} has  been served";
+        }
+
+        $ft = "Coffee";
+        $n = "4 cups";  // default parameter deoyar por o jodi argument set kore dei tahole se oi argument er value ke default parameter ar jaygay overwrite kore nibe
+
+
+       serve4($ft, $n);
+       echo "<hr>";
+    ?>
+
+
+
+
+
+<?php 
+        function serve5($foodType = "Coffee", $numberOfItems = "3 cup"){  // set a default parameter 
+            echo "{$numberOfItems} of {$foodType} have  been served";
+        }
+
+       serve5();
+       echo "<hr>";
     ?>
 </body>
 </html>
