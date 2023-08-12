7cdd94e83890dd2a29a9fae7e758b3e5bccbade1
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-08-12 19:16:39 +0600

    updated

--- Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/3_parameter_default_value.php
+++ Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/3_parameter_default_value.php
@@ -18,22 +18,26 @@
        echo "<hr>";
     ?>
 
-<h4> Function parameter default value or optional parameter </h4>
+
+
 <?php 
         function serve1($foodType, $numberOfItems){
             echo "{$numberOfItems} of {$foodType} have  been served";
         }
 
         $ft = "Coffee";
-        // $n = "";
+        $n = "";
 
        serve1($ft, $n);
        echo "<hr>";
     ?>
 
 
+
+
+<h4> Function parameter default value or optional parameter </h4>
 <?php 
-        function serve2($foodType, $numberOfItems = "1 cup"){  // set a default parameter 
+        function serve2($foodType, $numberOfItems = "1 cup"){  // set partial default parameter 
             echo "{$numberOfItems} of {$foodType} has  been served";
         }
 
@@ -46,13 +50,16 @@
 
 
 <?php 
-        function serve3($foodType = "Coffee", $numberOfItems = "3 cup"){  // set a default parameter 
+        function serve3($foodType = "Coffee", $numberOfItems = "3 cup"){  // set default parameter 
             echo "{$numberOfItems} of {$foodType} have  been served";
         }
 
        serve3();
        echo "<hr>";
     ?>
+
+
+
 
 <h4> Default parameter overwrite </h4>
 <?php 
@@ -71,14 +78,24 @@
 
 
 
-
 <?php 
         function serve5($foodType = "Coffee", $numberOfItems = "3 cup"){  // set a default parameter 
             echo "{$numberOfItems} of {$foodType} have  been served";
         }
 
-       serve5();
+       serve5("Salad", "2 Plated");
        echo "<hr>";
+    ?>
+
+
+
+<?php 
+    //     function serve6($foodType = "Coffee", $numberOfItems){  // set a default parameter 
+    //         echo "{$numberOfItems} of {$foodType} have  been served";
+    //     }
+
+    //    serve6("", 2 Plated");  // sudu 1 ta parameter ke default rekhe tar jonno akta argument pass kora jabe na. code error asbe.
+    //    echo "<hr>";
     ?>
 </body>
 </html>
