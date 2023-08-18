ecf7cf79c8b17643c270bc902e659be287058381
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-08-18 20:40:02 +0600

    updated

--- Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/9_variable_scope.php
+++ Ikbal_Hossain_1277699/Home_Practice/PHP_with_Hasin_Hayder/2_Function/9_variable_scope.php
@@ -1,5 +1,9 @@
+
 <h4>1. Basic Function using local scope </h4>
 <?php 
+
+    // scope = variable ar value kotokkhon porjonto valid, kothay valid and kon jaygay change kora jabe.
+
     function doSomething(){
         $name = "Earth";  // local scope
         echo $name;
@@ -12,10 +16,23 @@
 
 <h4>2. Problem of local scope in a function </h4>
 <?php 
+    function doSomething4(){
+        $name = "Earth";
+    }
+
+    doSomething4();
+    echo $name;
+?>
+
+
+
+
+<h4>3. Problem of local scope in a function </h4>
+<?php 
      $name = "Earth";
     function doSomething1(){
        
-        echo $name;  // result => Undefined variable: name bcz the variable $name = "Earth" is defined out of function not within function
+        echo $name;  // result => Undefined variable: name bcz the variable $name = "Earth" is defined out of function not within function.
     }
 
     doSomething1();
@@ -23,9 +40,11 @@
 
 
 
-<h4>3. Basic Function with global scope </h4>
-<?php 
-     $name = "Earth";
+
+<h4>4. Basic Function with global scope </h4>
+
+<?php  // 3 & 4 has been solved here
+     $name = "Earth";  // global scope
     function doSomething2(){
         global $name;  // global use korar karone php function ar bahire ei variable ke khujbe je ok declare kora hoyece kina. as this is true so it will give a valid output.
        
@@ -37,8 +56,10 @@
 
 
 
-<h4>4. How to use Super Global in function </h4>
-<?php 
+
+<h4>5. How to use Super Global in function </h4>
+<?php // 3 & 4 can be solved this way also
+
      $name = "Earth";
     function doSomething3(){
 
@@ -48,3 +69,6 @@
 
     doSomething3();
 ?>
+
+
+<h4>5. How to use Super Global in function </h4>
