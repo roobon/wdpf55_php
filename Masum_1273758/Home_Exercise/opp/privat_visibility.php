8ffc4ba726715ef6c48846b157b4160e4897dc63
masum-yasin <masum55549@gmail.com>
2023-08-25 19:11:52 -0700

    update

--- Masum_1273758/Home_Exercise/opp/privat_visibility.php
+++ Masum_1273758/Home_Exercise/opp/privat_visibility.php
@@ -1,29 +1,60 @@
 <?php 
-class country{
-  public $country ="Bangaldesh,Paksitan,maldiv,Nepal,India";
-  function getcountry(){
-    return $this->country;
-  } 
-  private $capital ="kulkata,Kathmundo,paris,turkey," ;
-  function addcapital(){
-    return $this->capital;
+// class country{
+//   public $country ="Bangaldesh,Paksitan,maldiv,Nepal,India";
+//   function getcountry(){
+//     return $this->country;
+//   } 
+//   private $capital ="kulkata,Kathmundo,paris,turkey," ;
+//   function addcapital(){
+//     return $this->capital;
+//   }
+// }
+// class anotherCountry extends country{
+//     function getcountry(){
+//         return $this->country;
+//       } 
+//       function addcapital(){
+//         return $this->capital;
+//       }
+// }
+// $newCountry=new country();
+// echo $newCountry->getcountry();
+// echo "<br>";
+// echo $newCountry->addcapital();
+// $anotherCountry=new anotherCountry();
+// echo "<br>";
+// echo $anotherCountry->getcountry();
+
+//   protected $country= "Bangladesh";
+//   public $address= "Maymansing";
+//   public function getName(){
+//     return " MY First Name is".$this->FirstName." And My second Name ".$this->secondName;
+//   } 
+
+use grandFather as GlobalGrandFather;
+
+  class grandFather {
+  private $FirstName = "Khayrul Alam";
+  private $address  ="Khulna";
+  protected $secondName = "Rayan";
+  public function getName(){
+    return "My name is".$this->FirstName.'<br>';
+  }
+
+}
+class personInfo extends grandFather{
+  public $name = "Mizanur Rahman";
+  public $age =25;
+ public function superperson(){
+    return"My name is".$this->name."And Second name".$this->secondName;
   }
 }
-class anotherCountry extends country{
-    function getcountry(){
-        return $this->country;
-      } 
-      function addcapital(){
-        return $this->capital;
-      }
-}
-$newCountry=new country();
-echo $newCountry->getcountry();
-echo "<br>";
-echo $newCountry->addcapital();
-$anotherCountry=new anotherCountry();
-echo "<br>";
-echo $anotherCountry->getcountry();
+$obj1 = new grandFather();
+echo $obj1->getName();
+$obj2 = new personInfo();
+echo $obj2->superperson();
+$outsidecallName = new personInfo();
+echo $outsidecallName->name;
 
 
 ?>
--- Masum_1273758/Home_Exercise/opp/protected_visibility.php
+++ Masum_1273758/Home_Exercise/opp/protected_visibility.php
@@ -1,52 +1,76 @@
 <?php 
-class person{
-protected $name="Khayrul Alam Rayan";
-    protected $age=25;
-    protected $country="Bangladesh";
-     function personInfo(){
-        return $this->name;}
-        function personInfo2(){
-            return $this->age;
-        }
-        function personInfo3(){
-            return $this->country;
-        }
+// class person{
+// protected $name="Khayrul Alam Rayan";
+//     protected $age=25;
+//     protected $country="Bangladesh";
+//      function personInfo(){
+//         return $this->name;}
+//         function personInfo2(){
+//             return $this->age;
+//         }
+//         function personInfo3(){
+//             return $this->country;
+//         }
+//     }
+//     class superPerson extends person{
+//         function personInfo(){
+//             return $this->name;}
+//             function personInfo2(){
+//                 return $this->age;
+//             }
+//             function personInfo3(){
+//                 return $this->country;
+//             }
+//             protected $district="Maymansing";
+
+//             function addInfo(){
+//                return $this->district;
+//             }
+//         }
+// $newPerson= new person();
+// echo $newPerson->personInfo();
+// echo "<br>";
+// echo $newPerson->personInfo2();
+// echo "<br>";
+// echo $newPerson->personInfo3();
+// $superPerson=new superPerson();
+// echo "<br>";
+// echo $superPerson->personInfo();
+// echo "<br>";
+
+// echo $superPerson->personInfo2();
+// echo "<br>";
+
+// echo $superPerson->personInfo3();
+// echo "<br>";
+
+// echo $superPerson->addInfo();
+// $outputofpersonvaluecall=new person();
+// $$outputofpersonvaluecallname->name;
+echo "<hr>";
+class grandFather{
+protected $name = "Khayrul Alam Rayan";
+protected $age = 26 ;
+protected $country = "Bangladesh";
+ function grandpa(){
+    return " My name is ".$this->name."I am ".$this->age."years old"." My Country Name is".$this->country;
+}
+}
+class Daddy extends grandFather{
+    public $name = "Nadia Sultana";
+    public $age = 30;
+     function superFather(){
+        return " My name is ".$this->name." I am ".$this->age." MY Country Name si ".$this->country;
     }
-    class superPerson extends person{
-        function personInfo(){
-            return $this->name;}
-            function personInfo2(){
-                return $this->age;
-            }
-            function personInfo3(){
-                return $this->country;
-            }
-            protected $district="Maymansing";
-
-            function addInfo(){
-               return $this->district;
-            }
-        }
-$newPerson= new person();
-echo $newPerson->personInfo();
-echo "<br>";
-echo $newPerson->personInfo2();
-echo "<br>";
-echo $newPerson->personInfo3();
-$superPerson=new superPerson();
-echo "<br>";
-echo $superPerson->personInfo();
-echo "<br>";
-
-echo $superPerson->personInfo2();
-echo "<br>";
-
-echo $superPerson->personInfo3();
-echo "<br>";
-
-echo $superPerson->addInfo();
-$outputofpersonvaluecall=new person();
-$$outputofpersonvaluecallname->name;
+}
+$obj1 = new grandFather();
+echo $obj1->grandpa();
+echo "<hr>";
+$obj2 = new Daddy();
+echo $obj2->superFather();
+echo "<hr>";
+$classoutcallgrandpasName = new Daddy();
+echo $classoutcallgrandpasName->name;
 
 
 
