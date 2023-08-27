fdf5b63cb9286728a7b71af17e72ddbaf4269c1f
masum-yasin <masum55549@gmail.com>
2023-08-27 13:30:05 +0600

    update

--- Masum_1273758/Home_Exercise/opp/inheritance.php
+++ Masum_1273758/Home_Exercise/opp/inheritance.php
@@ -1,24 +1,61 @@
 <?php 
-class member{
-    public $username="";
-    private $loggedIN=false;
-    public function login(){
-        $this->loggedIN=true;}
-        public function logout(){
-            $this->loggedIN=false;
-        }
-        public function isLoggedIn(){
-            return $this->loggedIN;
-        }
+// class Member{
+//     public $username="";
+//     private $loggedIN=false;
+//     public function login(){
+//         $this->loggedIN=true;}
+//         public function logout(){
+//             $this->loggedIN=false;
+//         }
+//         public function isLoggedIn(){
+//             return $this->loggedIN;
+//         }
     
-}
-class administrator extends member{
-    function createForum($forumName){
-        echo "$this->username create a forum.$forumName";
+// }
+// class administrator extends Member{
+//     function createForum($forumName){
+//         echo "$this->username create a forum:$forumName";
+//     }
+//     function extravalue($member){
+//         echo "$this->username extra member:$member->username";
+//     }
+// }
+// $member = new Member();
+// $member->username="Khayrul Alam Rayan";
+// $member->login();
+// echo $member->username."is".($member->isLoggedIn())?"loggedIN":"loggedout"."<br>";
+
+ 
+class information {
+    public $username = "";
+    private $loggedIN = false;
+
+    public function login() {
+        $this->loggedIN = true;
     }
-    function extravalue($member){
-        echo "$this->username extra member.$member->username";
+
+    public function logout() {
+        $this->loggedIN = false;
+    }
+
+    public function isLoggedIn() {
+        return $this->loggedIN;
     }
 }
 
+class Administrator extends information {
+    function createForum($forumName) {
+        echo "$this->username creates a forum: $forumName";
+    }
+
+    function extravalue($member) {
+        echo "$this->username extra member: $member->username";
+    }
+}
+
+$member = new information();
+$member->username = "Khayrul Alam Rayan";
+$member->login();
+
+echo $member->username . " is " . ($member->isLoggedIn() ? "logged in" : "logged out") . "<br>";
 ?>
