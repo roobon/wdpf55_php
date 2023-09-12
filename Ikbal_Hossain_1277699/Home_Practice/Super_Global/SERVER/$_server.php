a6dcf10124ef210966bf07078d18f56e43100b00
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-09-12 13:47:21 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Home_Practice/Super_Global/SERVER/$_server.php
@@ -0,0 +1,21 @@
+<?php
+ echo phpinfo();
+// here you will get php server version that you have installed in your local pc.
+// there you have variable in left side and value in right side
+// if you want to access the value with the help of variable then you need to type the variable name under echo
+?>
+
+<?php
+// echo $_SERVER['QUERY_STRING'];
+
+// http://localhost/wdpf55_php/Ikbal_Hossain_1277699/Home_Practice/Super_Global/SERVER/$_server.php?5
+// this is the url path. you can write anything after (.php) and that will be showed in browser.
+// you have to write (echo $_SERVER['QUERY_STRING'];) if you want to get the result 
+
+?>
+
+<?php
+    // echo $_SERVER['REQUEST_METHOD']
+    // echo $_SERVER['SCRIPT_FILENAME']
+   // echo $_SERVER['SERVER_ADDR'];
+?>
