52239a5998cb51467ccd5751435b6574f63f7818
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-09-12 08:58:43 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Home_Practice/Super_Global/POST/$_post.php
@@ -0,0 +1,5 @@
+<form action="action.php" method="POST">
+    Name: <input type="text" name="ename" placeholder="Enter your name">  <br> <br>
+    Mobile: <input type="text" name="mobile" placeholder="Enter your mobile"> <br> <br>
+    <input type="submit" name="submit" value="SUBMIT">
+</form>
---
+++ Ikbal_Hossain_1277699/Home_Practice/Super_Global/POST/action.php
@@ -0,0 +1,4 @@
+<?php 
+    echo "Name: " . $_POST['ename'];
+    echo "Number: " . $_POST['mobile'];
+?>
